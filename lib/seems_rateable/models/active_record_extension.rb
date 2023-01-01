7e5a5cde7fe99acffa8f511d37fc04b79993aa64
Bagaskara Wisnu Gunawan <bagaswisnu07@gmail.com>
2023-01-01 12:12:03 +0700

    Update active_record_extension.rb

--- lib/seems_rateable/models/active_record_extension.rb
+++ lib/seems_rateable/models/active_record_extension.rb
@@ -2,11 +2,11 @@ module SeemsRateable
   module Models
     module ActiveRecordExtension
       def seems_rateable(*dimensions)
-        has_many :_rates, -> { where dimension: nil }, as: :rateable, class_name: SeemsRateable::Rate, dependent: :destroy
+        has_many :_rates, -> { where dimension: nil }, as: :rateable, class_name: SeemsRateable::Rate.to_s, dependent: :destroy
         has_many :_raters, through: :_rates, source: :rater, dependent: :destroy
 
         dimensions.each do |dimension|
-          has_many :"#{dimension}_rates", -> { where dimension: dimension }, as: :rateable, class_name: SeemsRateable::Rate, dependent: :destroy
+          has_many :"#{dimension}_rates", -> { where dimension: dimension }, as: :rateable, class_name: SeemsRateable::Rate.to_s, dependent: :destroy
           has_many :"#{dimension}_raters", through: :"#{dimension}_rates", source: :rater, dependent: :destroy
         end
 
@@ -14,7 +14,7 @@ module SeemsRateable
       end
 
       def seems_rateable_rater
-        has_many :rates_given, class_name: SeemsRateable::Rate, foreign_key: :rater_id
+        has_many :rates_given, class_name: SeemsRateable::Rate.to_s, foreign_key: :rater_id
 
         include Rater
       end
